e29e99b4b2f3b9b67616da5e93f3ef62396b6758
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2023-11-04 01:42:13 +0300

    Fruit gun and its movement

--- Assets/Scripts/FruitGun.cs
+++ Assets/Scripts/FruitGun.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FruitGun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject fruit;
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject[] fruitPrefabs;
+    
+    private void Start()
     {
-        
+        var randomInt = Random.Range(0, fruitPrefabs.Length - 1);
+        var initialFruit = Instantiate(fruitPrefabs[randomInt], transform.position, Quaternion.identity);
+        initialFruit.transform.SetParent(transform);
+        fruit = initialFruit;
     }
 
-    // Update is called once per frame
+    private void Movement()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        if (transform.position.x is <= 3 and >= -3)
+        {
+            transform.Translate(new Vector3(horizontalInput * speed, 0, 0) * Time.deltaTime);
+        }
+    }
+
+    private void ShootFruit()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Fruit should have a method which it goes towards bottom.
+            // It seems physics is not applied until it makes a contact with another fruit.
+        }
+    }
+    
     void Update()
     {
-        
+        Movement();
     }
 }
